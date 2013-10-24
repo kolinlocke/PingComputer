ed2de1a64a11fc31ce3427947df139592839b305
kolinlocke <kolinlocke@gmail.com>
2013-10-24 11:59:39 +0800

    Initial Commit

---
+++ FrmMain.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace PingComputer
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMain));
+            this.Txt_Target = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Chk_Timeout = new System.Windows.Forms.CheckBox();
+            this.Btn_Ping = new System.Windows.Forms.Button();
+            this.Status = new System.Windows.Forms.StatusStrip();
+            this.Lbl_StatusMsg = new System.Windows.Forms.ToolStripStatusLabel();
+            this.Btn_Cancel = new System.Windows.Forms.Button();
+            this.Notify = new System.Windows.Forms.NotifyIcon(this.components);
+            this.Txt_Timeout = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Status.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Txt_Target
+            // 
+            this.Txt_Target.Location = new System.Drawing.Point(12, 29);
+            this.Txt_Target.Name = "Txt_Target";
+            this.Txt_Target.Size = new System.Drawing.Size(200, 20);
+            this.Txt_Target.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(154, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Target Computer IP/HostName";
+            // 
+            // Chk_Timeout
+            // 
+            this.Chk_Timeout.AutoSize = true;
+            this.Chk_Timeout.Location = new System.Drawing.Point(16, 55);
+            this.Chk_Timeout.Name = "Chk_Timeout";
+            this.Chk_Timeout.Size = new System.Drawing.Size(92, 17);
+            this.Chk_Timeout.TabIndex = 2;
+            this.Chk_Timeout.Text = "Has Timeout?";
+            this.Chk_Timeout.UseVisualStyleBackColor = true;
+            // 
+            // Btn_Ping
+            // 
+            this.Btn_Ping.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Btn_Ping.Location = new System.Drawing.Point(12, 78);
+            this.Btn_Ping.Name = "Btn_Ping";
+            this.Btn_Ping.Size = new System.Drawing.Size(80, 25);
+            this.Btn_Ping.TabIndex = 3;
+            this.Btn_Ping.Text = "PING!";
+            this.Btn_Ping.UseVisualStyleBackColor = true;
+            // 
+            // Status
+            // 
+            this.Status.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.Lbl_StatusMsg});
+            this.Status.Location = new System.Drawing.Point(0, 140);
+            this.Status.Name = "Status";
+            this.Status.Size = new System.Drawing.Size(284, 22);
+            this.Status.TabIndex = 4;
+            this.Status.Text = "statusStrip1";
+            // 
+            // Lbl_StatusMsg
+            // 
+            this.Lbl_StatusMsg.Name = "Lbl_StatusMsg";
+            this.Lbl_StatusMsg.Size = new System.Drawing.Size(62, 17);
+            this.Lbl_StatusMsg.Text = "StatusMsg";
+            // 
+            // Btn_Cancel
+            // 
+            this.Btn_Cancel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Btn_Cancel.Location = new System.Drawing.Point(98, 78);
+            this.Btn_Cancel.Name = "Btn_Cancel";
+            this.Btn_Cancel.Size = new System.Drawing.Size(100, 25);
+            this.Btn_Cancel.TabIndex = 5;
+            this.Btn_Cancel.Text = "Cancel Ping!";
+            this.Btn_Cancel.UseVisualStyleBackColor = true;
+            // 
+            // Notify
+            // 
+            this.Notify.Icon = ((System.Drawing.Icon)(resources.GetObject("Notify.Icon")));
+            this.Notify.Text = "Ping Computer!";
+            this.Notify.Visible = true;
+            // 
+            // Txt_Timeout
+            // 
+            this.Txt_Timeout.Location = new System.Drawing.Point(114, 53);
+            this.Txt_Timeout.Name = "Txt_Timeout";
+            this.Txt_Timeout.Size = new System.Drawing.Size(98, 20);
+            this.Txt_Timeout.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(215, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "secs.";
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 162);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Txt_Timeout);
+            this.Controls.Add(this.Btn_Cancel);
+            this.Controls.Add(this.Status);
+            this.Controls.Add(this.Btn_Ping);
+            this.Controls.Add(this.Chk_Timeout);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Txt_Target);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "FrmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Ping Computer!";
+            this.Status.ResumeLayout(false);
+            this.Status.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Txt_Target;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox Chk_Timeout;
+        private System.Windows.Forms.Button Btn_Ping;
+        private System.Windows.Forms.StatusStrip Status;
+        private System.Windows.Forms.Button Btn_Cancel;
+        private System.Windows.Forms.ToolStripStatusLabel Lbl_StatusMsg;
+        private System.Windows.Forms.NotifyIcon Notify;
+        private System.Windows.Forms.TextBox Txt_Timeout;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ FrmMain.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net.NetworkInformation;
+using System.Net;
+
+namespace PingComputer
+{
+    public partial class FrmMain : Form
+    {
+        #region _Variables
+
+        Ping mPing = new Ping();
+        String mAddress;
+        Int32 mTimeout;
+        const Int32 CnsTimeout = 5000;
+
+        #endregion
+
+        #region _Constructor
+
+        public FrmMain()
+        {
+            InitializeComponent();
+
+            this.Lbl_StatusMsg.Text = "Ready";
+            this.Btn_Ping.Enabled = true;
+            this.Btn_Cancel.Enabled = false;
+
+            this.Txt_Timeout.Text = "5";
+
+            this.Btn_Ping.Click += new EventHandler(Btn_Ping_Click);
+            this.Btn_Cancel.Click += new EventHandler(Btn_Cancel_Click);
+            this.Chk_Timeout.CheckedChanged += new EventHandler(Chk_Timeout_CheckedChanged);
+            this.mPing.PingCompleted += new PingCompletedEventHandler(mPing_PingCompleted);
+        }
+
+        #endregion
+
+        #region _EventHandlers
+
+        void Btn_Ping_Click(object sender, EventArgs e)
+        {
+            this.StartPing();
+        }
+
+        void Btn_Cancel_Click(object sender, EventArgs e)
+        {
+            this.CancelPing();
+        }
+
+        void Chk_Timeout_CheckedChanged(object sender, EventArgs e)
+        {
+            this.Txt_Timeout.Enabled = this.Chk_Timeout.Checked;
+        }
+
+        void mPing_PingCompleted(object sender, PingCompletedEventArgs e)
+        {
+            if (e.Cancelled)
+            {
+                MessageBox.Show(
+                        "Pinging Cancelled"
+                        , "Ping Cancelled"
+                        , MessageBoxButtons.OK
+                        , MessageBoxIcon.Information);
+            }
+            else if (e.Error != null)
+            {
+                MessageBox.Show(
+                        "An Error Occurred, please check the IP Address or Host Name and try again."
+                        , "Error Occurred"
+                        , MessageBoxButtons.OK
+                        , MessageBoxIcon.Error);
+            }
+            else if (e.Reply != null)
+            {
+                if (e.Reply.Status == IPStatus.Success)
+                {
+                    this.Notify.ShowBalloonTip(
+                        5000
+                        , "Ping Finished"
+                        , "Pinging " + this.Txt_Target.Text + " Successful!"
+                        , ToolTipIcon.Info);
+
+                    this.Lbl_StatusMsg.Text = "Pinging " + this.Txt_Target.Text + " Successful!";
+                }
+                else
+                {
+                    if (!this.Chk_Timeout.Checked)
+                    {
+                        this.Ping();
+                        return;
+                    }
+                    else
+                    {
+                        switch (e.Reply.Status)
+                        {
+                            case IPStatus.TimedOut:
+                                this.Notify.ShowBalloonTip(
+                                    5000
+                                    , "Ping Timeout"
+                                    , "Pinging " + this.Txt_Target.Text + " Failed due to timeout!"
+                                    , ToolTipIcon.Warning);
+
+                                this.Lbl_StatusMsg.Text = "Pinging " + this.Txt_Target.Text + " Failed due to timeout!";
+
+                                break;
+                            default:
+                                this.Notify.ShowBalloonTip(
+                                    5000
+                                    , "Ping Failed"
+                                    , "Pinging " + this.Txt_Target.Text + " Failed!"
+                                    , ToolTipIcon.Warning);
+
+                                this.Lbl_StatusMsg.Text = "Pinging " + this.Txt_Target.Text + " Failed!";
+
+                                break;
+                        }
+                    }
+                }
+            }
+            else
+            { this.Lbl_StatusMsg.Text = "Unknown Event Occured. Please Try Again."; }
+
+            //this.Lbl_StatusMsg.Text = "Ready";
+
+            this.Btn_Ping.Enabled = true;
+            this.Btn_Cancel.Enabled = false;
+            this.Chk_Timeout.Enabled = true;
+            this.Txt_Target.Enabled = true;
+            this.Txt_Timeout.Enabled = true;
+        }
+
+        #endregion
+
+        #region _Methods
+
+        void StartPing()
+        {
+            this.mAddress = this.Txt_Target.Text;
+            this.mTimeout = (Convert_Int32(this.Txt_Timeout.Text, 5) * 1000);
+            if (this.mTimeout <= 0)
+            {
+                MessageBox.Show("Warning", "Timeout must be greater than zero!", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return;
+            }
+
+            this.Btn_Ping.Enabled = false;
+            this.Btn_Cancel.Enabled = true;
+            this.Chk_Timeout.Enabled = false;
+            this.Txt_Target.Enabled = false;
+            this.Txt_Timeout.Enabled = false;
+
+            this.Ping();
+
+            this.Lbl_StatusMsg.Text = "Pinging...";
+        }
+
+        void CancelPing()
+        {
+            this.mPing.SendAsyncCancel();
+        }
+
+        void Ping()
+        {
+            try
+            { this.mPing.SendAsync(this.mAddress, CnsTimeout, new Object()); }
+            catch
+            {
+                MessageBox.Show(
+                        "An Error Occurred, please check the IP Address or Host Name and try again."
+                        , "Error Occurred"
+                        , MessageBoxButtons.OK
+                        , MessageBoxIcon.Error);
+            }
+
+        }
+
+        public static object IsNull(object Obj_Input, object Obj_NullOutput)
+        {
+            if (Obj_Input == null || Object.Equals(DBNull.Value, Obj_Input))
+            { return Obj_NullOutput; }
+            else
+            { return Obj_Input; }
+        }
+
+        public static Int32 Convert_Int32(Object Value)
+        { return Convert_Int32(Value, 0); }
+
+        public static Int32 Convert_Int32(Object Value, Int32 DefaultValue)
+        {
+            string ValueString = string.Empty;
+            if (Value != null)
+            {
+                try { ValueString = Value.ToString(); }
+                catch { }
+            }
+
+            Int32 ReturnValue;
+            if (!Int32.TryParse(ValueString, out ReturnValue))
+            { ReturnValue = DefaultValue; }
+            return ReturnValue;
+        }
+
+        #endregion
+    }
+}
